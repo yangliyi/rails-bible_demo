2fa9116d9b254f52b069c07a111238cdeaf2a71d
Yang Li-Yi <joseph.lyyang@gmail.com>
2015-09-16 18:11:15 +0800

    Add page Add flash Add error message Add partial Add layout

--- app/controllers/events_controller.rb
+++ app/controllers/events_controller.rb
@@ -1,14 +1,16 @@
 class EventsController < ApplicationController
 
+  before_action :set_event, :only => [:show, :edit, :update, :destroy]
+
   #GET /events/index
   #GET /events
   def index
-    @events = Event.all
+    @events = Event.page(params[:page]).per(5)
   end
 
   #GET /events/show/:id
   def show
-    @event = Event.find( params[:id] )
+    @page_title = @event.name
   end
 
 
@@ -22,33 +24,42 @@ class EventsController < ApplicationController
   def create
     @event = Event.new(event_params)
 
-    @event.save
+    if @event.save
+      flash[:notice] = "event was successfully created"
+      redirect_to :action => :index # HTTP code: 303 重發 GET
+    else
+      render :action => :new #new.html.erb
+    end
 
-    redirect_to :action => :index # HTTP code: 303 重發 GET
   end
 
   # GET /events/edit/:id
   def edit
-    @event = Event.find( params[:id] )
+
   end
 
   #POST /events/update/:id
   def update
-    @event = Event.find(params[:id])
 
-    @event.update(event_params)
-
-    redirect_to :action => :show, :id => @event
+    if @event.update(event_params)
+      flash[:notice] = "event was successfully updated"
+      redirect_to :action => :show, :id => @event
+    else
+      # redirect_to :action => :edit, :id => @event
+      render :action => :edit # edit.html.erb
+    end
   end
 
   #GET /events/destroy/:id
   def destroy
-    @event = Event.find(params[:id])
-
     @event.destroy
-
+    flash[:alert] = "event was successfully deleted"
     redirect_to :action => :index # HTTP code: 303 重發 GET
 
+  end
+
+  def set_event
+    @event = Event.find(params[:id])
   end
 
   private
